70beaa084fb3f8fe6b6f0f25a181a97d9ef4dc5c
somnath <mandal.somnath1234@gmail.com>
2023-09-03 12:29:37 +0530

    #

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import MenuItems from './MenuItems';
+import Link from 'next/link';
+import {AiFillHome} from "react-icons/ai";
+import { BsFillInfoCircleFill} from "react-icons/bs";
+export default function Header() {
+  return (
+    <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
+        <div className=" flex">
+                <MenuItems title="HOME" address="/" Icon={AiFillHome}/>
+                <MenuItems title="ABOUT" address="/address" Icon={BsFillInfoCircleFill}/>
+        </div>
+        <div className="">
+          {/* <DarkModeSwitch/> */}
+            <Link href="/">
+              <h2 className='text-2xl'>
+                <span className=' font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>IMDb</span>
+                <span className='text-xl hidden sm:inline'>Clone</span>
+              </h2>
+            </Link>
+        </div>
+    </div>
+  )
+}
