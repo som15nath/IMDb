f7ccea47f3ec1a7362a52dd0b9203ab9d9cb2ad1
somnath <mandal.somnath1234@gmail.com>
2023-09-11 12:19:11 +0530

    create the navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Providers from "./Providers";
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
           <Header />
 
           {/* Navbar */}
-
+            <Navbar/>
           {/* SearchBox */}
 
           {children}
